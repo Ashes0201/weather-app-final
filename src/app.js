eddf7042ebd5c97c662e9aa7aae78141d5441296
Ashes0201 <ashleighabraham@live.com>
2022-01-12 18:48:56 -0800

    added search form and updated javascript innerhtml

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response);
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let temperatureElement = document.querySelector("#temperature");
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
 
+let apiKey = "d702c598912f6b831e35edf8569da794";
+let city = "las vegas";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
