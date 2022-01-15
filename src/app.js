bc49843bad824c97a5e5aa0c7b772704047ebed9
Ashes0201 <ashleighabraham@live.com>
2022-01-14 20:29:56 -0800

    added unit conversion

--- src/app.js
+++ src/app.js
@@ -31,11 +31,13 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+  fahrenheitTemperature = response.data.main.temp;
+
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
@@ -55,6 +57,31 @@ function handleSearch(event) {
   let cityInput = document.querySelector("#city-input");
   search(cityInput.value);
 }
+
+function displayCelsiusTemp(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+function displayFahrenheitTemp(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+let fahrenheitTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 let searchForm = document.querySelector("#form-search");
 searchForm.addEventListener("submit", handleSearch);
